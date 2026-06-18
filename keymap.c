fa9edeccbc7899e3a59e70a473e417142cc01d55
systemadminster <104146123+systemadminster@users.noreply.github.com>
2026-06-18 22:46:09 +0900

    Add files via upload

---
+++ keymap.c
@@ -0,0 +1,28 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,
+        KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MS_BTN1,
+        KC_RGUI, MO(1),   KC_SPC,  KC_TAB,  KC_PGUP, MS_BTN2, KC_PGDN
+    ),
+    [1] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
+        KC_RSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_INS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_BTN1,
+        KC_NO,   KC_NO,   KC_SPC,  KC_LGUI, KC_PGUP, MS_BTN3, KC_PGDN
+    ),
+    [2] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_BTN1,
+        KC_LGUI, KC_NO,   KC_SPC,  KC_LGUI, KC_PGUP, KC_TRNS, KC_PGDN
+    ),
+    [3] = LAYOUT(
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    )
+};
