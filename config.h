9a931d1579121f0cf5bdf6e2a15176aa93c55265
systemadminster <104146123+systemadminster@users.noreply.github.com>
2026-06-19 23:24:36 +0900

    Update config.h

--- config.h
+++ config.h
@@ -1,11 +1,9 @@
 #pragma once
 
-// Matrix size for cocot46lp (physical pins are in BMP's own keyboard.json)
 #undef MATRIX_ROWS
 #undef MATRIX_COLS
 #define MATRIX_ROWS 4
-#define MATRIX_COLS 7
+#define MATRIX_COLS 32
 #define DIODE_DIRECTION COL2ROW
 
-// pimoroni_trackball (AZ1UBALL)
 #define PIMORONI_TRACKBALL_INTERVAL_MS 8
