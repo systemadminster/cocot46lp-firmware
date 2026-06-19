b5eb46308d1ac094d053bff6acb0613fddd959a1
systemadminster <104146123+systemadminster@users.noreply.github.com>
2026-06-19 22:41:11 +0900

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,23 +1,37 @@
 #include QMK_KEYBOARD_H
 
+// cocot46lp: 4 rows x 7 cols
+#undef LAYOUT
+#define LAYOUT( \
+    k00, k01, k02, k03, k04, k05, k06, \
+    k10, k11, k12, k13, k14, k15, k16, \
+    k20, k21, k22, k23, k24, k25, k26, \
+    k30, k31, k32, k33, k34, k35, k36  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06 }, \
+    { k10, k11, k12, k13, k14, k15, k16 }, \
+    { k20, k21, k22, k23, k24, k25, k26 }, \
+    { k30, k31, k32, k33, k34, k35, k36 }  \
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,
-        KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MS_BTN1,
-        KC_RGUI, MO(1),   KC_SPC,  KC_TAB,  KC_PGUP, MS_BTN2, KC_PGDN
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MS_BTN1, MS_BTN2,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    MS_BTN3, KC_NO,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_NO,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(1),   KC_NO,   KC_NO
     ),
     [1] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
-        KC_RSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_INS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_BTN1,
-        KC_NO,   KC_NO,   KC_SPC,  KC_LGUI, KC_PGUP, MS_BTN3, KC_PGDN
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_RSFT, KC_INS,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO
     ),
     [2] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
-        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_BTN1,
-        KC_LGUI, KC_NO,   KC_SPC,  KC_LGUI, KC_PGUP, KC_TRNS, KC_PGDN
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LSFT, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO
     ),
     [3] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
