0426e163e07400e103c124dd5d4cba270fda1eb4
systemadminster <104146123+systemadminster@users.noreply.github.com>
2026-06-19 22:41:52 +0900

    Update config.h

--- config.h
+++ config.h
@@ -1,22 +1,11 @@
 #pragma once
 
-// USB
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
-#define MANUFACTURER "aki27"
-#define PRODUCT      "cocot46lp"
-
-// Matrix: 4 rows x 7 cols
+// Matrix size for cocot46lp (physical pins are in BMP's own keyboard.json)
+#undef MATRIX_ROWS
+#undef MATRIX_COLS
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 7
-#define MATRIX_ROW_PINS { 9, 10, 47, 46 }
-#define MATRIX_COL_PINS { 35, 32, 23, 22, 20, 19, 18 }
 #define DIODE_DIRECTION COL2ROW
-
-// AZ1UBALL I2C (SDA=D1=GPIO5, SCL=D0=GPIO6)
-#define I2C1_SDA_PIN 5
-#define I2C1_SCL_PIN 6
 
 // pimoroni_trackball (AZ1UBALL)
 #define PIMORONI_TRACKBALL_INTERVAL_MS 8
